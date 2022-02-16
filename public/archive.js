0e9aa804788233739bdaec4634b450307e0e59fb
coekuss <45906331+coekuss@users.noreply.github.com>
2022-02-15 23:42:25 -0500

    separate archive data in separate file

---
+++ public/archive.js
@@ -0,0 +1,178 @@
+const archiveData = {
+  "PROJECTS": {
+    "2020": {
+      "Virtual Ascension": {
+        "thumbnails": [
+          "archive/projects/2020 - Virtual Ascension/1.vaproduct.jpg",
+          "archive/projects/2020 - Virtual Ascension/2.vaproduct1.jpg",
+          "archive/projects/2020 - Virtual Ascension/3.vaskndlss1.jpg",
+          "archive/projects/2020 - Virtual Ascension/4.vataboo1.jpg"
+        ],
+        "full": [
+          "archive/projects/2020 - Virtual Ascension/full/1.vaproduct.jpg",
+          "archive/projects/2020 - Virtual Ascension/full/2.vaproduct1.jpg",
+          "archive/projects/2020 - Virtual Ascension/full/3.vaskndlss1.jpg",
+          "archive/projects/2020 - Virtual Ascension/full/4.vataboo1.jpg"
+        ],
+        "description": "In collaboration with SKNDLSS"
+      }
+    },
+    
+    "2019": {
+      "Bae Blade": {
+        "thumbnails": [
+          "archive/projects/2019 - Bae Blade/1.baefront.jpg",
+          "archive/projects/2019 - Bae Blade/2.baeback.jpg",
+          "archive/projects/2019 - Bae Blade/3.trade.jpg"
+        ],
+        "full": [
+          "archive/projects/2019 - Bae Blade/full/1.baefront.jpg",
+          "archive/projects/2019 - Bae Blade/full/2.baeback.jpg",
+          "archive/projects/2019 - Bae Blade/full/3.trade.jpg"
+        ],
+        "description": "In collaboration with SKNDLSS"
+      },
+      "Mirrored Collection": {
+        "thumbnails": [
+          "archive/projects/2019 - Mirrored Collection/1.MirrorCollection.jpg",
+          "archive/projects/2019 - Mirrored Collection/2.TribalHairClip.jpg",
+          "archive/projects/2019 - Mirrored Collection/3.TabooHairClip.jpg",
+          "archive/projects/2019 - Mirrored Collection/4.NinjaStar.jpg",
+          "archive/projects/2019 - Mirrored Collection/5.NinjaStarEarrings.jpg"
+        ],
+        "full": [
+          "archive/projects/2019 - Mirrored Collection/full/1.MirrorCollection.jpg",
+          "archive/projects/2019 - Mirrored Collection/full/2.TribalHairClip.jpg",
+          "archive/projects/2019 - Mirrored Collection/full/3.TabooHairClip.jpg",
+          "archive/projects/2019 - Mirrored Collection/full/4.NinjaStar.jpg",
+          "archive/projects/2019 - Mirrored Collection/full/5.NinjaStarEarrings.jpg"
+        ],
+        "description": "In collaboration with Michele Yue"
+      }
+    },
+    "2018": {
+      "Taboo Juggalo Shirt": {
+        "thumbnails": [
+          "archive/projects/2018 - Taboo Juggalo Shirt/1.shirt.jpg",
+          "archive/projects/2018 - Taboo Juggalo Shirt/2.lookbook4.jpg",
+          "archive/projects/2018 - Taboo Juggalo Shirt/3.lookbook1.jpg",
+          "archive/projects/2018 - Taboo Juggalo Shirt/4.lookbook3.jpg"
+        ],
+        "full": [
+          "archive/projects/2018 - Taboo Juggalo Shirt/full/1.shirt.jpg",
+          "archive/projects/2018 - Taboo Juggalo Shirt/full/2.lookbook4.jpg",
+          "archive/projects/2018 - Taboo Juggalo Shirt/full/3.lookbook1.jpg",
+          "archive/projects/2018 - Taboo Juggalo Shirt/full/4.lookbook3.jpg"
+        ],
+        "description": "Shot by Yuliss Benitez. Art by Isaidah E. Cruz."
+      },
+      "Taboo World Stickers": {
+        "thumbnails": [
+          "archive/projects/2018 - Taboo World Stickers/1.Sticker.jpg"
+        ],
+        "full": [
+          "archive/projects/2018 - Taboo World Stickers/full/1.Sticker.jpg"
+        ],
+        "description": ""
+      },
+      "Taboo World Patches": {
+        "thumbnails": [
+          "archive/projects/2018 - Taboo World Patches/3.Patch.jpg"
+        ],
+        "full": [
+          "archive/projects/2018 - Taboo World Patches/full/3.Patch.jpg"
+        ],
+        "description": ""
+      }
+    }
+  },
+  "CAMPAIGNS": {
+    "2022": {
+      "Taboo Liner Reloaded Onyx Black": {
+        "thumbnails": [
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/1.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/2.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/3.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/4.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/5.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/6.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/7.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/8.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/9.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/10.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/11.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/12.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/13.jpg"
+        ],
+        "full": [
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/1.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/2.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/3.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/4.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/5.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/6.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/7.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/8.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/9.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/10.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/11.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/12.jpg",
+          "archive/campaigns/2022 - Taboo Liner Reloaded Onyx Black/full/13.jpg"
+        ],
+        "description": "Shot by Brian Vu"
+      }
+    },
+    "2021": {
+      "Taboo Glue Pen": {
+        "thumbnails": [
+          "archive/campaigns/2021 - Taboo Glue Pen/tabooglue.jpg",
+          "archive/campaigns/2021 - Taboo Glue Pen/tabooglue3.jpg",
+          "archive/campaigns/2021 - Taboo Glue Pen/tabooglue4.jpg"
+        ],
+        "full": [
+          "archive/campaigns/2021 - Taboo Glue Pen/full/tabooglue.jpg",
+          "archive/campaigns/2021 - Taboo Glue Pen/full/tabooglue3.jpg",
+          "archive/campaigns/2021 - Taboo Glue Pen/full/tabooglue4.jpg"
+        ],
+        "description": "Shot by Chris Vu"
+      }
+    }
+  },
+  "PRESS": {
+    "2021": {
+      "Vogue": {
+        "thumbnails": [
+          "archive/press/2021 - Vogue/3.vogue2021.jpg",
+          "archive/press/2021 - Vogue/4.vogue2021.jpg",
+          "archive/press/2021 - Vogue/5.vogue2021.jpg"
+        ],
+        "full": [
+          "archive/press/2021 - Vogue/full/3.vogue2021.jpg",
+          "archive/press/2021 - Vogue/full/4.vogue2021.jpg",
+          "archive/press/2021 - Vogue/full/5.vogue2021.jpg"
+        ],
+        "description": "Taboo Liner by Ryan Burke"
+      }
+    },
+    "2019": {
+      "King Kong Magazine": {
+        "thumbnails": [
+          "archive/press/2019 - King Kong Magazine/kingkong.jpg"
+        ],
+        "full": [
+          "archive/press/2019 - King Kong Magazine/full/kingkong.jpg"
+        ],
+        "description": "Shot by Brian Vu"
+      },
+      "Depop": {
+        "thumbnails": [
+          "archive/press/2019 - Depop/2.depop2019.jpg"
+        ],
+        "full": [
+          "archive/press/2019 - Depop/full/2.depop2019.jpg"
+        ],
+        "description": ""
+      }
+    }
+  }
+}
